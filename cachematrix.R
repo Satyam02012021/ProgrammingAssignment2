5f41b9e708135c4ed30bd35592bf030f7892d077
Satyam Pal <112113924+Satyam02012021@users.noreply.github.com>
2024-04-19 14:00:15 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+#set the value of the vector
+#get the value of the vector
+#set the value of the mean
+#get the value of the mean
+
+
 makeVector <- function(x = numeric()) {
     vec <- x
     set <- function(value) {
@@ -7,4 +13,31 @@ makeVector <- function(x = numeric()) {
     mean <- function() mean(vec)
     list(set = set, get = get, mean = mean)
 }
- 
+
+
+
+
+
+
+
+
+
+#Caching the Inverse of a Matrix
+
+# Initialize a variable to store the cached inverse
+cached_inverse <- NULL
+
+# Function to calculate the inverse of a matrix with caching
+calculate_inverse <- function(matrix) {
+  if (!is.null(cached_inverse)) {
+    # If the cached inverse exists, return it
+    message("Using cached inverse")
+    return(cached_inverse)
+  } else {
+    # If the cached inverse doesn't exist, calculate it
+    message("Calculating inverse")
+    inverse <- solve(matrix)
+    cached_inverse <<- inverse  # Cache the inverse
+    return(inverse)
+  }
+}
